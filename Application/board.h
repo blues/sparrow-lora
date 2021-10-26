e3e7fea1d2d3887aa0fc4875ed51f7e388f4b5c5
Ray Ozzie <ray@ozzie.net>
2021-10-26 17:26:43 -0400

    flip bme power back to being PP on V2

--- Application/board.h
+++ Application/board.h
@@ -12,7 +12,7 @@
 
 #define BOARD_NUCLEO    0           // NUCLEO-WL55JC1
 #define BOARD_V1        1           // v1.0 spin of board
-#define BOARD_V2        2           // v1.1 change of bme280 to low-side switching
+#define BOARD_V2        2           // v1.1 
 #define CURRENT_BOARD   BOARD_V1
 
 // All pins on the STM32WLE5 UFQFPN48 package
