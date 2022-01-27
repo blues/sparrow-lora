f2246ae79eef63714921dd4a09b91e20ae674e40
Zachary J. Fields <zachary_fields@yahoo.com>
2022-01-27 18:14:34 +0000

    feat: Incorporate GitHub CI

--- cmake-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-[ -d cmake-build-debug ] || mkdir cmake-build-debug
-[ -d note-c ] || git clone https://github.com/blues/note-c
-cd cmake-build-debug
-cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug .. && cmake --build . -- -j 4
