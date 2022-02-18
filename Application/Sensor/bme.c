9ea1294b6ce6f3f6d027be36e93a3a8e4f9a452c
Zachary J. Fields <zachary_fields@yahoo.com>
2022-02-18 18:34:07 +0000

    fix: SPARROW_BOARD --> SPARROW_DEVICE

--- Application/Sensor/bme.c
+++ Application/Sensor/bme.c
@@ -240,7 +240,7 @@ static bool addNote()
                (int) lastBME.humidity, (int) (fabs(lastBME.humidity*100)) % 100);
 
     // Add the voltage, just for convenient reference
-#if (SPARROW_BOARD != BOARD_NUCLEO)
+#if (SPARROW_DEVICE != BOARD_NUCLEO)
     JAddNumberToObject(body, "voltage", MX_ADC_A0_Voltage());
 #endif
 
